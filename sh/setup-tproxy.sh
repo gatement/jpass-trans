43b7fca6f56418982a53df0acddc4ee5b2fc00c8
Johnson Lau <gatement@gmail.com>
2020-04-16 07:26:29 -0400

    go

--- sh/setup-tproxy.sh
+++ sh/setup-tproxy.sh
@@ -1,13 +1,21 @@
+#> vim /etc/sysctl.conf
+#    net.ipv4.ip_forward = 1
+#> sysctl -p
+#> cat /proc/sys/net/ipv4/ip_forward
+
 iptables -t filter -P FORWARD ACCEPT
 iptables -t filter -F
 iptables -t mangle -F
 # ssh
 iptables -t mangle -A PREROUTING -p tcp -d 192.168.56.0/24 -j ACCEPT
+iptables -t mangle -A PREROUTING -p udp -d 192.168.56.0/24 -j ACCEPT
 # gateway
 iptables -t mangle -A PREROUTING -p tcp -d 10.0.3.2/32 -j ACCEPT
+iptables -t mangle -A PREROUTING -p udp -d 10.0.3.2/32 -j ACCEPT
 # tproxy
-iptables -t mangle -A PREROUTING -p udp -j TPROXY --on-port 8116 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -p tcp -j TPROXY --on-port 8117 --tproxy-mark 0x2333/0x2333 
+iptables -t mangle -A PREROUTING -p tcp -j TPROXY --on-port 8116 --tproxy-mark 1
+iptables -t mangle -A PREROUTING -p udp -j TPROXY --on-port 53 --tproxy-mark 1
+#iptables -t mangle -A PREROUTING -p udp -j TPROXY --on-port 53 --tproxy-mark 0x2333/0x2333 
 
 # tcp
 ip rule add fwmark 1 lookup 100
@@ -15,8 +23,8 @@ ip route add local 0.0.0.0/0 dev lo table 100
 #ip route add local 0.0.0.0/0 dev lo tab local
 
 # udp
-ip rule add fwmark 0x2333/0x2333 pref 101 table 101
-ip route add local default dev lo table 101
+#ip rule add fwmark 0x2333/0x2333 pref 101 table 101
+#ip route add local default dev lo table 101
 
 #ip rule add fwmark 0x2333/0x2333 pref 101 table 101
 #ip route add local default dev lo table 101
